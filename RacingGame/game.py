c810f30e0b0427d9885be6e462fbcf4678723e74
EmilianoJO <jimenezortizemiliano1@gmail.com>
2023-10-10 08:27:00 -0600

    Tests

--- RacingGame/game.py
+++ RacingGame/game.py
@@ -2,95 +2,105 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 import time
 
-#Instanciamos la clase Ursina
+# Instanciamos la clase Ursina
 app = Ursina()
 
-#Player
+# Player
 player = FirstPersonController(
     collider='box',
     jump_height=0,
     speed=30
 )
-#Player position at the start
-player.position = (0,10,-20)
-#Cursor invisible
+# Player position at the start
+player.position = (0, 10, -20)
+# Cursor invisible
 player.cursor.visible = False
 
-#Cielo oscurecido
+# Cielo oscurecido
 sky = Sky()
 sky.texture = 'sky_sunset'
 
-#Piso
+# Piso
 ground = Entity(
     model='plane',
     texture='street.jpg',
-    collider = 'mesh',
-    scale=(10,-2,50),
+    collider='mesh',
+    scale=(10, -2, 50),
+)
+ground1 = Entity(
+    position=(0, 0, 50),
+    model='plane',
+    texture='street.jpg',
+    collider='mesh',
+    scale=(10, -2, 50),
 )
 
 
-
-#Pared
+# Pared
 pillar0 = Entity(
     model='cube',
-    texture = 'concrete.jpg',
-    scale=(10,1.2,.1),
-    position=(0,.5,-25),
+    texture='fence2.jpg',
+    scale=(10, 1.2, .1),
+    position=(0, .5, -25),
     collider='box'
 )
 pillar1 = Entity(
     model='cube',
-    texture = 'concrete.jpg',
-    scale=(.1,1.2,50),
-    position=(5,.5,0),
+    texture='fence2.jpg',
+    scale=(.1, 1.2, 50),
+    position=(5, .5, 0),
     collider='box'
 )
 pillar2 = Entity(
     model='cube',
-    texture = 'concrete.jpg',
-    scale=(.1,1.2,50),
-    position=(-5,.5,0),
+    texture='fence2.jpg',
+    scale=(.1, 1.2, 50),
+    position=(-5, .5, 0),
     collider='box'
 )
 
-#Finish line
+# Finish line
 finish_line = Entity(
     model='cube',
-    texture = 'finish_line.jpg',
-    scale=(10,1.2,.1),
-    position=(0,10,25),
+    texture='finish_line.jpg',
+    scale=(10, 1.2, .1),
+    position=(0, 10, 25),
     collider='box'
 )
 
+image = Sprite('assets/f1car.png', scale=(2, 2, .1), parent=camera.ui)
+
+
 def update():
-    
-    #Block RIGHT and LEFT movement
-    input_handler.bind('a','l')
-    input_handler.bind('b','b')
+
+    # Block RIGHT and LEFT movement
+    input_handler.bind('a', 'l')
+    input_handler.bind('d', 'b')
 
 
-#Close button game
+# Close button game
 def input(key):
-  if key == 'escape':
+    if key == 'escape':
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("Seconds:",round(elapsed_time,2))
-    
-    #Sleep for 1 seconds, and print time before quitting
-    #new_time = Text(text='Time:'+str(round(elapsed_time,2)),position=(-0.8,0.4),scale=2,color=color.black)
-    time.sleep(1)
-    
-    quit()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Seconds:", round(elapsed_time, 2))
+
+        # Sleep for 1 seconds, and print time before quitting
+        # new_time = Text(text='Time:'+str(round(elapsed_time,2)),position=(-0.8,0.4),scale=2,color=color.black)
+        time.sleep(1)
+
+        quit()
 
 
-#Start game
+# Start game
 start_time = time.time()
-#elapsed_time = 0
-#Print realtime on screen
-time_text = Text(text='Time:',position=(-0.8,0.4),scale=2,color=color.black)
+# elapsed_time = 0
+# Print realtime on screen
+time_text = Text(text='Time:', position=(-0.8, 0.4),
+                 scale=2, color=color.black)
 
-#Audio
-audio = Audio('assets/TokyoDrift.mp3',loop=True, autoplay=True)
+# Audio
+# audio = Audio('assets/TokyoDrift.mp3',loop=True, autoplay=True)
 
 app.run()
