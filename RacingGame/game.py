d2f33145389b9f979601708125edb0882272bb9d
EmilianoJO <jimenezortizemiliano1@gmail.com>
2023-11-04 17:34:57 -0600

    Fixes

--- RacingGame/game.py
+++ RacingGame/game.py
@@ -15,9 +15,9 @@ player = FirstPersonController(
     speed=5
 )
 
-#Player position at the start
-player.position = (0,10,-20)
-#Cursor invisible
+# Player position at the start
+player.position = (0, 10, -20)
+# Cursor invisible
 player.cursor.visible = False
 player.cursor.enabled = False
 
@@ -85,19 +85,17 @@ neutral_audio = Audio('assets/ferrari-488-pista-neutral.mp3',
 acce_audio = Audio('assets/ferrari-488-pista-acceleration.mp3',
                    loop=False, autoplay=False)
 
-
-image = Sprite('assets/f1car.png', scale=(2, 2, .1), parent=camera.ui)
-
-car  = Entity(
-    parent = camera.ui,
-    model = 'cube',
-    position = (0,0),
-    scale = (1.8,1,1),
-    texture = 'assets/ferrari1pixel.png'
+car = Entity(
+    parent=camera.ui,
+    model='cube',
+    position=(0, 0),
+    scale=(1.8, 1, 1),
+    texture='assets/ferrari1pixel.png'
 )
-#make a global velocity variable
+# make a global velocity variable
 velocity = 0
-player_og_speed =5
+player_og_speed = 5
+
 
 def update():
 
@@ -108,94 +106,93 @@ def update():
 
     if held_keys['w'] and not acc_audio.playing:
         neutral_audio.stop()
-        acc_audio.play()    
-    
+        acc_audio.play()
+
     if held_keys['q'] and held_keys['w']:
 
-        if velocity<50:
-            velocity +=0.1
+        if velocity < 50:
+            velocity += 0.1
         player.speed = player_og_speed + velocity
-        
+
         car.texture = 'assets/ferrari1pixel.png'
         car.texture = 'assets/ferrari2pixel.png'
-        mouse.position -= (0.001,0,0.001)
-        
-    
+        mouse.position -= (0.001, 0, 0.001)
+
     elif held_keys['e'] and held_keys['w']:
-        
-        if velocity<50:
-            velocity +=0.1
+
+        if velocity < 50:
+            velocity += 0.1
         player.speed = player_og_speed + velocity
 
         car.texture = 'assets/ferrari1pixel.png'
         car.texture = 'assets/ferrari3pixel.png'
-        mouse.position += (0.001,0,0.001)
-
+        mouse.position += (0.001, 0, 0.001)
 
     elif not held_keys['w'] and not neutral_audio.playing:
         acc_audio.stop()
         neutral_audio.play()
-        
+
     elif held_keys['e'] and held_keys['s']:
-        
-        #velocity -=0.01
-    
+
+        # velocity -=0.01
+
         car.texture = 'assets/ferrari1pixel.png'
         car.texture = 'assets/ferrari3pixel.png'
-        mouse.position += (0.001,0,0.001)
+        mouse.position += (0.001, 0, 0.001)
 
     elif held_keys['q'] and held_keys['s']:
 
-        #velocity -=0.01
+        # velocity -=0.01
 
         car.texture = 'assets/ferrari1pixel.png'
         car.texture = 'assets/ferrari2pixel.png'
-        mouse.position -= (0.001,0,0.001)
+        mouse.position -= (0.001, 0, 0.001)
 
-    elif held_keys['q'] :
-        
-        #print("presionando q")
+    elif held_keys['q']:
+
+        # print("presionando q")
         car.texture = 'assets/ferrari1pixel.png'
         car.texture = 'assets/ferrari2pixel.png'
-        
+
     elif held_keys['e']:
-        #mouse.position += (0.001,0,0.001)
+        # mouse.position += (0.001,0,0.001)
         car.texture = 'assets/ferrari1pixel.png'
         car.texture = 'assets/ferrari3pixel.png'
 
     elif held_keys['s']:
-        
-        #velocity -=0.01
-        
+
+        # velocity -=0.01
 
         car.texture = 'assets/ferrari1pixel.png'
-    
+
     elif held_keys['w']:
 
-        if velocity<50:
-            velocity +=0.1
+        if velocity < 50:
+            velocity += 0.1
         player.speed = player_og_speed + velocity
-        
+
         car.texture = 'assets/ferrari1pixel.png'
-    
+
     elif held_keys['space']:
         velocity = 0
         car.texture = 'assets/ferrari1pixel.png'
-    
+
     else:
-        #print("nada")
-        print("Velocidad:",velocity)
-        
-        if velocity>0:
-            velocity-=1
-        if velocity<0:
-            velocity=0
-    
+        # print("nada")
+        print("Velocidad:", velocity)
+
+        if velocity > 0:
+            velocity -= 1
+        if velocity < 0:
+            velocity = 0
+
     player.speed = player_og_speed + velocity
 
 # mouse.locked= True
 
 # Close button game
+
+
 def input(key):
     if key == 'escape':
 
@@ -212,12 +209,13 @@ def input(key):
 
 start_time = time.time()
 
-time_text = Text(text='Time:',position=(-0.8,0.4),scale=2,color=color.black)
-#time_text.create_background(padding=(.5,.25),radius=Text.size/2)
+time_text = Text(text='Time:', position=(-0.8, 0.4),
+                 scale=2, color=color.black)
+# time_text.create_background(padding=(.5,.25),radius=Text.size/2)
 
 
 # Audio
-audioSong = Audio('assets/TokyoDrift.mp3',loop=True, autoplay=False)
+audioSong = Audio('assets/TokyoDrift.mp3', loop=True, autoplay=False)
 mouse.locked = True
 # Start game
 app.run()
